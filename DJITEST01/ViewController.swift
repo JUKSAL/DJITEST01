29346bb66a8290d4fb2171517c2cf3160accdbbb
JUKSAL <jukka.salmenkyla@gmail.com>
2020-08-08 18:28:34 +0300

    Initial Commit

---
+++ DJITEST01/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DJITEST01
+//
+//  Created by Jukka Salmenkylä on 08/08/20.
+//  Copyright © 2020 Jukka Salmenkylä. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
